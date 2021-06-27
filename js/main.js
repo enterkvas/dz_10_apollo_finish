0a805c432e25b5b3e0c6e295b51543467a5128dd
enterkvas <alexanderbugakov66@gmail.com>
2021-06-27 12:00:19 +0300

    Second commit: to the slider structure

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Инициализация Swiper
+new Swiper('.slider__swiper', {
+	// Стрелки:
+	/*
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	*/
+	slidesPerView: 1,
+});
