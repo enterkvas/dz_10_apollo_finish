ea45063355d9ebb303fb63b37bc72b3d2643072e
enterkvas <alexanderbugakov66@gmail.com>
2021-09-18 21:08:35 +0300

    Third commit: the navigation block for 320 is ready

--- js/script.js
+++ js/script.js
@@ -1,108 +1,31 @@
 "use strict";
-// Инициализация Swiper
-new Swiper('.slider-swiper', {
-	// Стрелки:	
-	navigation: {
-		nextEl: '.button--next',
-		prevEl: '.button--prev'
-	},
-	// Буллеты 
-	// pagination: {
-	// 	el: '.swiper-pagination',
-	// 	clickable: true,
-	// },	
-	slidesPerView: 1,
-});
-// Код, который помогает определить на каком устр-ве открыта наша стр:
-const isMobile = {
-	Android: function () {
-		return navigator.userAgent.match(/Android/i);
-	},
-	BlackBerry: function () {
-		return navigator.userAgent.match(/BlackBerry/i);
-	},
-	iOS: function () {
-		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-	},
-	Opera: function () {
-		return navigator.userAgent.match(/Opera Mini/i);
-	},
-	Windows: function () {
-		return navigator.userAgent.match(/IEMobile/i);
-	},
-	any: function () {
-		return (
-			isMobile.Android() ||
-			isMobile.BlackBerry() ||
-			isMobile.iOS() ||
-			isMobile.Opera() ||
-			isMobile.Windows() 
-		); 
-	}
-};
-// ---------------------------------------------
-// Выбор девайса (mobile or pc):
-if (isMobile.any()) {
-	document.body.classList.add('_touch');
-} else {
-	document.body.classList.add('_pc');
-}
-//---------------------------------------------
-// Меню бургер
 const nav = document.querySelector('.navigation');
-const iconBurger = document.querySelector('.menu__burger-button');
-const navList = document.querySelector('.menu__body');
-if (iconBurger) {	
-	iconBurger.addEventListener("click", function(e) {
-		// document.body.classList.toggle('_lock');
-		iconBurger.classList.toggle('_active');
-		navList.classList.toggle('_active');
-		nav.classList.toggle('_active');		
-	});
-}
-// ---------------------------------------------
-// Добавление/удаление класса _active:
-// if (isMobile.any()) {
-// 	document.body.classList.add('_touch');
+const iconBurger = document.querySelector('.menu__burger--button');
+const navList = document.querySelector('.menu__list');
+iconBurger.addEventListener("click", function(e) {	
+	navList.classList.toggle('_active');
+	navList.classList.toggle('container--mobile-no-padding');
+	nav.classList.toggle('navigation--navy-blue');		
+});
 
-// 	let menuArrows = document.querySelectorAll('.menu__burger-button');
-// 	if (menuArrows.lenght > 0) {
-// 		for (let index = 0; index < menuArrows.length; index++) {
-// 			const menuArrow = menuArrows[index];
-// 			menuArrow.addEventListener("click", function(e) {
-// 				menuArrow.parentElement.classList.toggle('_active');
-// 			});
-// 		}
-// 	}
-
-// } else {
-// 	document.body.classList.add('_pc');
+// Показ\Скрытие разных текстовок слайдера на разных разрешениях:
+// const w = document.body.clientWidth();
+// const visibleMobile = document.querySelector('.visible-mobile');
+// if (w < 992) {
+// 	navList.classList.remove('_active');	
 // }
-// ---------------------------------------------
-// Прокрутка при клике
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if (menuLinks.length > 0) {
-	menuLinks.forEach(menuLink => {
-		menuLink.addEventListener("click", onMenuLinkClick);
-	});
-	function onMenuLinkClick(e) {
-		const menuLink = e.target;
-		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
-			const gotoBlock = document.querySelector(menuLink.dataset.goto);
-			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
 
-			// if (iconMenu.classList.contains('_active')) {
-			// 	document.body.classList.remove('_lock');
-			// 	iconMenu.classList.remove('_active');
-			// 	menuBody.classList.remove('_active');
-			// }
 
-			window.scrollTo({
-				top: gotoBlockValue,
-				behavior: "smooth" /* указывает ПЛАВНУЮ прокрутку */
-			});
-			e.preventDefault(); /* отменяем стандартное поведение ссылки */
-		}
-	}
-}
-
+// Инициализация Swiper:
+new Swiper('.slider-swiper', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	// Пагинация:
+	pagination: {
+		el: '.swiper-pagination',
+		// Буллеты:
+		clickable: true,
+	},
+});
